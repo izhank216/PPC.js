a404180e638dbcb3acd7bdaeb7757f58f3fd0b1c
izhank216 <izhank216@gmail.com>
2026-01-09 23:53:45 +0500

    Create isaconfig.js

---
+++ src/isaconfig.js
@@ -0,0 +1,12 @@
+module.exports = {
+  vendorId: 0x8086,
+  deviceId: 0x7110,
+  command: 0x0000,
+  status: 0x0280,
+  revisionId: 0x01,
+  progIf: 0x00,
+  subclass: 0x06,
+  classCode: 0x06,
+  headerType: 0x00,
+  bist: 0x00
+}
